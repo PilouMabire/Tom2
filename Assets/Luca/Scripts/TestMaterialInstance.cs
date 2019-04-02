75d54e97f33b954ffdc889e753962ec266fbe3d8
Pierre-Louis <p.mabire@rubika-edu.com>
2019-04-02 12:07:22 +0200

    Recomi lucas

---
+++ Assets/Luca/Scripts/TestMaterialInstance.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMaterialInstance : MonoBehaviour
+{
+
+    public Material mat;
+    Material _mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mat = Instantiate<Material>(mat);
+        GetComponent<Renderer>().material = _mat;
+        _mat.SetColor("_Additional", Color.red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
