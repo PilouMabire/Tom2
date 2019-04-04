12ecc9b53785e0e9e319c1903f6cb731aa93b546
LucaFerrantelli <lucas.ferrantelli@gmail.com>
2019-04-04 15:25:10 +0200

    IntegrationParc

--- Assets/Luca/Scripts/Chien/EnteringParc.cs
+++ Assets/Luca/Scripts/Chien/EnteringParc.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class EnteringParc : MonoBehaviour
 {
     public Chien_Laisse chien;
+    public CameraFollow cam;
+    public GameObject goToCam;
+
+    public GameObject player;
+    public GameObject playerGoTo;
 
     // Start is called before the first frame update
     void Start()
@@ -20,11 +25,20 @@ public class EnteringParc : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        chien = other.GetComponent<Chien_Laisse>();
-        if(chien)
+        if(other.GetComponent<Chien_Laisse>())
         {
             chien.isAttatchedToPlayer = false;
         }
+
+        if (other.GetComponent<Player3DExample>())
+        {
+            cam.followPlayer = false;
+            cam.StartCoroutine(cam.CinematicTo(goToCam));
+            cam.objectToFollow = goToCam;
+            player.transform.position = playerGoTo.transform.position;
+            chien.gameObject.transform.position = playerGoTo.transform.position + new Vector3(1, 0, 1);
+        }
+
 
     }
 }
---
+++ Assets/Luca/Scripts/Chien/ParcToForest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParcToForest : MonoBehaviour
+{
+    public Baboule_Controller stick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player3DExample>() && stick.iterations >= 2)
+        {
+            
+        }
+    }
+}
