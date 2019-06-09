fa72ec0c9176400f0329b0da580e2e15e2ff64a8
LucaFerrantelli <lucas.ferrantelli@gmail.com>
2019-06-09 12:08:03 +0200

    LastScene

---
+++ Assets/Luca/Scripts/Maison/GetItemOff.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetItemOff : MonoBehaviour
+{
+    public GameObject setParent;
+    public GameObject setActive;
+    public GameObject setInactive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<Player3DExample>())
+        {
+            setParent.transform.parent = null;
+            setActive.SetActive(true);
+            setInactive.SetActive(false);
+        }
+    }
+}
